620bc86e87b0b852882699572b2f45a83d0b78c0
Grace <gracemwangi846@gmail.com>
2025-01-06 09:40:17 +0300

    first commit

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
-def greet(name):
+
+
+def greet_programmer(name="programmer"):
+    print(f"Hello, {name}!")
     pass
+
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+
+
+def greet_with_default(name="programmer"):
+    print ("Hello, programmer!")
     pass
 
+
 def add(num1, num2):
+    num1=45
+    num2=55
+    return num1 + num2
     pass
 
 def halve(number):
+    return number/2
     pass
